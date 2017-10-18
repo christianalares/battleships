e754d736c51be9906ad13e1b0e851fea240728d2
Christian Alares <christian.alares@gmail.com>
2017-10-18 14:31:58 +0200

    Animating the sea (not done)

--- webpack.config.js
+++ webpack.config.js
@@ -7,56 +7,62 @@ const DIST_DIR = path.resolve(__dirname, 'dist')
 const SRC_DIR  = path.resolve(__dirname, 'src')
 
 module.exports = {
-    devtool: 'eval-source-map',
-    // entry: {
-    //     app: './src/index.js'
-    // },
-    entry: [
-        'webpack-hot-middleware/client?reload=true',
-        SRC_DIR + '/index.js'
-    ],
-    output: {
-        path: DIST_DIR,
-        filename: 'bundle.js',
-        publicPath: '/static/'
-    },
-    plugins: [
-        new webpack.optimize.OccurrenceOrderPlugin(),
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.DefinePlugin({
-            'process.env.NODE_ENV': JSON.stringify('development')
-        })
-    ],
-    module: {
-        rules: [
-            { 
-                test: /\.js$/,
-                exclude: /(node_modules)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['react', 'env', 'stage-2']
-                    }
-                }
-            },
-            {
-                test: /\.json$/,
-                use: { loader: 'json' }
-            },
-            {
-                test: /\.css$/,
-                use: { loader: 'style-loader' }
-            },
-            {
-                test: /\.css$/,
-                use: {
-                    loader: 'css-loader',
-                    query: {
-                        modules: true,
-                        localIdentName: '[name]__[local]___[hash:base64:5]'
-                    }
-                }
-            }
-        ]
-    }
+	devtool: 'eval-source-map',
+	// entry: {
+	//     app: './src/index.js'
+	// },
+	entry: [
+		'webpack-hot-middleware/client?reload=true',
+		SRC_DIR + '/index.js'
+	],
+	output: {
+		path: DIST_DIR,
+		filename: 'bundle.js',
+		publicPath: '/static/'
+	},
+	plugins: [
+		new webpack.optimize.OccurrenceOrderPlugin(),
+		new webpack.HotModuleReplacementPlugin(),
+		new webpack.DefinePlugin({
+			'process.env.NODE_ENV': JSON.stringify('development')
+		})
+	],
+	module: {
+		rules: [
+			{ 
+				test: /\.js$/,
+				exclude: /(node_modules)/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['react', 'env', 'stage-2']
+					}
+				}
+			},
+			{
+				test: /\.json$/,
+				use: { loader: 'json' }
+			},
+			{
+				test: /\.css$/,
+				use: { loader: 'style-loader' }
+			},
+			{
+				test: /\.css$/,
+				use: {
+					loader: 'css-loader',
+					query: {
+						modules: true,
+						localIdentName: '[name]__[local]___[hash:base64:5]'
+					}
+				}
+			},
+			{
+				test: /\.(png|svg|jpg|gif)$/,
+				use: [
+					'file-loader'
+				]
+			}
+		]
+	}
 }
